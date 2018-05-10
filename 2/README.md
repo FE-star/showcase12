b644169fe5433b4e4af21de878faa33178103f65
kael <yorts52@qq.com>
2018-05-10 22:04:54 +0800

    Step 2

---
+++ 2/README.md
@@ -0,0 +1,15 @@
+# Step 2
+
+## 添加日志
+```bash
+npm i log4js
+```
+
++ 参考 https://log4js-node.github.io/log4js-node/
+
+## 添加 watch 支持
+```bash
+npm install -g nodemon
+```
+
++ 修改 npm script
---
+++ 2/index.js
@@ -0,0 +1,43 @@
+/*
+ * @Author: kael 
+ * @Date: 2018-05-10 21:11:34 
+ * @Last Modified by: kael
+ * @Last Modified time: 2018-05-10 22:01:17
+ */
+
+const logger = require('./utils/logger')();
+logger.info(`Bootstrapping application`);
+
+const Koa = require('koa');
+const app = new Koa();
+
+const compress = require('koa-compress')({ flush: 2 });
+const conditional = require('koa-conditional-get')();
+const bodyparser = require('koa-bodyparser')();
+const etag = require('koa-etag')();
+
+app.keys = ['appkeys'];
+
+app
+  .use(conditional)
+  .use(etag)
+  .use(compress)
+  .use(bodyparser)
+
+const fs = require('fs');
+const path = require('path');
+const Router = require('koa-router');
+((root = './routers') => {
+  fs.readdirSync(root).forEach(filename => {
+    let file = path.parse(filename);
+    logger.info('load router:', filename);
+    if (file.ext.toLowerCase() !== '.js') return;
+    const router = new Router({ prefix: `/${file.name}` });
+    require(`${root}/${file.name}`)(router);
+    app.use(router.routes());
+  });
+})();
+
+app.listen(3000);
+
+logger.info('Server listening on http://localhost:3000');
