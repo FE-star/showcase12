51dea52a3f8ae3d41767b8693d0220ad98256896
yorts52 <yorts52@qq.com>
2018-08-19 19:10:05 +0800

    fix: update env port

--- 5/config/index.js
+++ 5/config/index.js
@@ -2,7 +2,7 @@
  * @Author: kael 
  * @Date: 2018-05-11 17:28:48 
  * @Last Modified by: kael
- * @Last Modified time: 2018-05-11 19:03:19
+ * @Last Modified time: 2018-08-19 19:09:34
  */
 
 const logger = require('../utils/logger')();
@@ -21,7 +21,7 @@ module.exports = (env) => {
 
   // 配置通过环境变量注入
   if (process.env.PORT) {
-    conf.port = 8000;
+    conf.port = process.env.PORT;
   }
 
   return conf;
